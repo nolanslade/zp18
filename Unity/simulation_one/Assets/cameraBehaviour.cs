b9a7577005fcb2aa2e6a9ae3e5f803f9fda30d0e
nolanslade <nolanslade@gmail.com>
2018-11-20 15:41:51 -0500

    Script for camera

---
+++ Unity/simulation_one/Assets/cameraBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehaviour : MonoBehaviour {
+
+    //private GameObject physicalCamera = GameObject.FindGameObjectWithTag("physicalCamera");
+    const float UNIT_RATIO = 1 / 25.0f;    // Ratio of Camera object 1M to Unity units. E.g. 1m =
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GameObject physicalCamera = GameObject.FindGameObjectWithTag("physicalCamera");
+
+        this.gameObject.transform.position = new Vector3 (
+            physicalCamera.transform.position.x / UNIT_RATIO,
+            physicalCamera.transform.position.y / UNIT_RATIO,
+            physicalCamera.transform.position.z / UNIT_RATIO
+        );
+	}
+}
