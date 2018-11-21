211bc98b0c92982316f8492e07d144b4e42e4fe5
nolanslade <nolanslade@gmail.com>
2018-11-21 11:38:27 -0500

    Refactoring scene

---
+++ Unity/simulation_one/Assets/Scripts/cameraBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehaviour : MonoBehaviour {
+
+    //private GameObject physicalCamera = GameObject.FindGameObjectWithTag("physicalCamera");
+    const float UNIT_RATIO = 2.5f / 65.0f;    // Ratio of Camera object 1M to Unity units. E.g. 1m =
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update relative to absolute time
+	void FixedUpdate () {
+
+        GameObject physicalCamera = GameObject.FindGameObjectWithTag("physicalCamera");
+
+        transform.position = new Vector3 (
+            physicalCamera.transform.localPosition.x / UNIT_RATIO,
+            physicalCamera.transform.localPosition.y / UNIT_RATIO,
+           physicalCamera.transform.localPosition.z / UNIT_RATIO
+        );
+    
+	}
+}
