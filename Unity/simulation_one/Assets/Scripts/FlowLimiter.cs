7318611b74ab8d39bf7fc305f820991101a3a0bb
nolanslade <nolanslade@gmail.com>
2019-01-19 11:12:02 -0500

    Flow limiter gameobject in scene + script

---
+++ Unity/simulation_one/Assets/Scripts/FlowLimiter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowLimiter : MonoBehaviour {
+
+	public GameObject flowManager;
+	public GameObject simManager;
+
+	private void OnTriggerEnter (Collider col) {
+		if (simManager.GetComponent<SimManager>().currentState() == SimManager.GameState.RUNNING) {
+			flowManager.GetComponent<FlowManager>().startFlow();
+		}
+	}
+
+	private void OnTriggerExit (Collider col) {
+		flowManager.GetComponent<FlowManager>().stopFlow();
+	}
+}
