d8b990a12db46eac649291745b66dfcc7261e979
unknown <nolanslade@gmail.com>
2019-02-14 19:57:52 +0000

    Removing update function from UI Update and permanently attached corresponding game objects in scene. Both UIs now working this way

--- Unity/simulation_one/Assets/Scripts/UIUpdate.cs
+++ Unity/simulation_one/Assets/Scripts/UIUpdate.cs
@@ -11,14 +11,6 @@ public class UIUpdate : MonoBehaviour {
     public GameObject moneyText;
     public UnityEngine.UI.Text text;
 
-	// Use this for initialization
-	void Start () {
-        simManager = GameObject.Find("SimManager");
-        timeRemainingText = GameObject.Find("TimeRemainingAmount");
-        dayText = GameObject.Find("DayAmount");
-        moneyText = GameObject.Find("Money");
-    }
-
     // Update is called once per frame
     void Update () {
         timeRemainingText.GetComponent<UnityEngine.UI.Text>().text = simManager.GetComponent<SimManager>().getElapsedDayTime().ToString();
