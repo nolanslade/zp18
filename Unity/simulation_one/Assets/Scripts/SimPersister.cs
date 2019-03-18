c97bd69ca0fee39bf1eccbe058e3b534e5d022be
nolanslade <nolanslade@gmail.com>
2019-03-18 10:04:29 -0400

    updating format string length for new params

--- Unity/simulation_one/Assets/Scripts/SimPersister.cs
+++ Unity/simulation_one/Assets/Scripts/SimPersister.cs
@@ -27,7 +27,7 @@ public class SimPersister {
     private const string LOG_FILE_PATT  = "yyyy-MMM-dd_HH-mm-ss";
     private const string HEAD_DATE_PATT = "yyyy-MMM-dd HH:mm";
     private const string LOG_FILE_SUFF  = ".txt";
-    private const string TXT_OUTPUT_FMT = "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22},{23},{24},{25},{26},{27},{28}";
+    private const string TXT_OUTPUT_FMT = "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22},{23},{24},{25},{26},{27},{28},{29},{30},{31},{32}";
 
     private System.IO.StreamWriter fileWriter;
 
