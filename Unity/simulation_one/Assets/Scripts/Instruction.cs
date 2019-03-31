1befcbb2133ae7564903227b6be914109f47faae
nolanslade <nolanslade@gmail.com>
2019-03-31 13:47:35 -0400

    Adding overloaded constructor + new bool field for sound

--- Unity/simulation_one/Assets/Scripts/Instruction.cs
+++ Unity/simulation_one/Assets/Scripts/Instruction.cs
@@ -13,10 +13,18 @@ public class Instruction {
 
     public string message;
     public float displayDuration;
+    public bool playSoundAtStart;  // Will play a countdown at the start of the display duration
 
     public Instruction (string m, float d)
     {
         this.message = m;
         this.displayDuration = d;
+        this.playSoundAtStart = false;
+    }
+
+    public Instruction (string m, float d, bool p) {
+    	this.message = m;
+        this.displayDuration = d;
+        this.playSoundAtStart = p;
     }
 }
