45b793406d4a5a8c259bcb9b154b4ce8ba196130
Nolan Slade <nolanslade@gmail.com>
2019-04-22 16:40:33 -0400

    Parsing hacky-ness --;

--- Unity/simulation_one/Assets/Scripts/ConfigKeyword.cs
+++ Unity/simulation_one/Assets/Scripts/ConfigKeyword.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 /**
  * McDSL: VR Simulation One
@@ -9,57 +9,53 @@
  */
 public sealed class ConfigKeyword {
 
-    // Auxiliary
-    public static readonly string COMMENT       = "#";
-    public static readonly string SEPARATOR     = ":";
-    public static readonly string INDENT        = "\t";
-    public static readonly string VALUE_START   = "\"";
-    public static readonly string VALUE_END     = "\"";
-    public static readonly string PERCENT       = "%";
-    public static readonly string F_SLASH       = "/";
+    char[] delimiter = { '\n', ':', '\t', '#', ' ', '%', ',' };
+    public static readonly char NEWLINE             = '\n';
+    public static readonly char COLON               = ':';
+    public static readonly char TAB                 = '\t';
+    public static readonly char HASH                = '#';
+    public static readonly char SPACE               = ' ';
+    public static readonly char PERCENTAGE          = '%';
+    public static readonly char COMMA               = ',';
 
-    // Top-level keywords
-    public static readonly string SIMULATION    = "Simulation";
-    public static readonly string TUTORIAL      = "Tutorial";
-    public static readonly string DAY           = "Day";
+    public static readonly string COMMENT           = "#";
+    public static readonly string SEPARATOR         = ":";
+    public static readonly string INDENT            = "\t";
+    public static readonly string VALUE_START       = "\"";
+    public static readonly string VALUE_END         = "\"";
+    public static readonly string PERCENT           = "%";
+    public static readonly string F_SLASH           = "/";
+    public static readonly string SIMULATION        = "Simulation";
+    public static readonly string TUTORIAL          = "Tutorial";
+    public static readonly string DAY               = "Day";
+    public static readonly string NAME              = "Name";           // Configuration name/id
+    public static readonly string OUTPUT            = "Output";         // Output type specification
+    public static readonly string DESCRIPTION       = "Description";    // For simulation
+    public static readonly string INSTRUCTIONS      = "Instructions";   // For simulation instrunctions
+    public static readonly string SOUND             = "Sound";          // For simulation sound
+    public static readonly string SCENE             = "Scene";          // For simulation scene
+    public static readonly string SCORE             = "Score";          // For day 0 score (unimpaired)
+    public static readonly string IMP_SCORE         = "ImpairedScore";  // For second stage of day 0 (optional) - impaired threshold
+    public static readonly string DURATION          = "Duration";       // For a given day
+    public static readonly string IMPAIRMENT        = "Impairment";     // For a given day
+    public static readonly string TREATMENT         = "Treatment";      // For a given day
+    public static readonly string BALLVALUE         = "BallValue";      // For a given day 
+    public static readonly string TYPE              = "Type";           // For a given impairment
+    public static readonly string STRENGTH          = "Strength";         // For a given impairment
+    public static readonly string FOG_IMP           = "Visual/Fog";
+    public static readonly string SHAKE_IMP         = "Physical/Shake";
+    public static readonly string WAIT              = "Wait";           // For a given treatment - 
+    public static readonly string CERTAINTY         = "Certainty";      // For a given treatment -
+    public static readonly string COST              = "Cost";           // For a given treatment - in dollars / reward units
+    public static readonly string EFFECTIVENESS     = "Effectiveness";  // For a given treatment - 
+    public static readonly string C                 = "C";              //
+    public static readonly string a                 = "a";              //
+    public static readonly string b                 = "b";
+    public static readonly string c                 = "c";
+    public static readonly string PROBABILITY       = "Probability";
+    public static readonly string EFFECT            = "Effect";
+    public static readonly string DEFAULT           = "Default";
+    public static readonly string DISABLED          = "Disabled";
+    public static readonly string ENABLED           = "Enabled";
 
-    // Key-value tier 2 keywords
-    public static readonly string NAME          = "Name";           // Configuration name/id
-    public static readonly string OUTPUT        = "Output";         // Output type specification
-    public static readonly string DESCRIPTION   = "Description";    // For simulation
-    public static readonly string INSTRUCTIONS  = "Instructions";   // For simulation instrunctions
-    public static readonly string SOUND         = "Sound";          // For simulation sound
-    public static readonly string SCENE         = "Scene";          // For simulation scene
-
-    public static readonly string SCORE         = "Score";          // For day 0 score (unimpaired)
-    public static readonly string IMP_SCORE     = "ImpairedScore";  // For second stage of day 0 (optional) - impaired threshold
-
-    public static readonly string DURATION      = "Duration";       // For a given day
-    public static readonly string IMPAIRMENT    = "Impairment";     // For a given day
-    public static readonly string TREATMENT     = "Treatment";      // For a given day
-    public static readonly string WATERVALUE    = "WaterValue";     // For a given day 
-
-
-    // Key-value tier 3 keywords
-    public static readonly string TYPE          = "Type";           // For a given impairment
-    public static readonly string STRENGTH      = "Strength";         // For a given impairment
-    public static readonly string FOG_IMP       = "Visual/Fog";
-    public static readonly string SHAKE_IMP     = "Physical/Shake";
-
-    // Key-value, tier 4
-    public static readonly string WAIT          = "Wait";           // For a given treatment - 
-    public static readonly string CERTAINTY     = "Certainty";      // For a given treatment -
-    public static readonly string COST          = "Cost";           // For a given treatment - in dollars / reward units
-    public static readonly string EFFECTIVENESS = "Effectiveness";  // For a given treatment - 
-
-    //Key-value, tier 5
-    public static readonly string C             = "C";              //
-    public static readonly string a             = "a";              //
-    public static readonly string b             = "b";
-    public static readonly string c             = "c";
-
-    public static readonly string PROBABILITY = "Probability";
-    public static readonly string EFFECT      = "Effect";
 }
-
-
